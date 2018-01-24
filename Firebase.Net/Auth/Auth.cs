bb9c1d8d64448961ce2aa666dbdc302f95eb71d3
Rémi Pelletier <remi.pelletier@polymtl.ca>
2018-01-24 15:19:48 -0500

    Added error handling and refactored

--- Firebase.Net/Auth/Auth.cs
+++ Firebase.Net/Auth/Auth.cs
@@ -6,18 +6,20 @@ namespace Firebase.Net.Auth
 {
     public class Auth
     {
-        private static class Constants
+        private static class Endpoints
         {
-            public static string BaseEndpoint = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/";
+            public static readonly string ApiBase = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/";
+            public static readonly string AccountInfo = "getAccountInfo";
+            public static readonly string SignUp = "signupNewUser";
+            public static readonly string SignIn = "verifyPassword";
+            public static readonly string SendConfirmationEmail = "getOobConfirmationCode";
+            public static readonly string PasswordReset = "resetPassword";
+        }
 
-            public static string AccountInfoEndpoint = "getAccountInfo";
-            public static string SignUpEndpoint = "signupNewUser";
-            public static string SignInEndpoint = "verifyPassword";
-            public static string SendConfirmationEmailEndpoint = "getOobConfirmationCode";
-            public static string PasswordResetEndpoint = "resetPassword";
-
-            public static string PasswordResetRequest = "PASSWORD_RESET";
-            public static string VerifyEmailRequestes = "VERIFY_EMAIL";
+        private static class RequestTypes
+        {
+            public static readonly string PasswordReset = "PASSWORD_RESET";
+            public static readonly string VerifyEmail= "VERIFY_EMAIL";
         }
 
         private string ApiKey { get; set; }
@@ -28,94 +30,139 @@ namespace Firebase.Net.Auth
         {
             ApiKey = apiKey;
             Client = new HttpClient();
-            Client.BaseAddress = new Uri(Constants.BaseEndpoint);
+            Client.BaseAddress = new Uri(Endpoints.ApiBase);
         }
 
         private async Task<AccountInfo> GetAccountInfo(string idToken)
         {
-            string url = Constants.AccountInfoEndpoint + EndpointKeySuffix;
+            string url = Endpoints.AccountInfo + EndpointKeySuffix;
             var body = new { idToken };
             var response = await Client.PostAsJsonAsync(url, body);
-            if (response.IsSuccessStatusCode)
+            if (!response.IsSuccessStatusCode)
             {
-                return await response.Content.ReadAsAsync<AccountInfo>();
+                var error = await response.Content.ReadAsAsync<Error>();
+                if (error != null)
+                    throw error.GetCorrespondingException();
+
+                throw new FirebaseAuthException();
             }
-            return null;
+            return await response.Content.ReadAsAsync<AccountInfo>();
         }
 
         public async Task<User> CreateUserWithEmailAndPassword(string email, string password)
         {
-            string url = Constants.SignUpEndpoint + EndpointKeySuffix;
+            string url = Endpoints.SignUp + EndpointKeySuffix;
             var body = new { email, password, returnSecureToken = true };
             var response = await Client.PostAsJsonAsync(url, body);
-            if (response.IsSuccessStatusCode)
+            if (!response.IsSuccessStatusCode)
             {
-                var signUpInfo = await response.Content.ReadAsAsync<SignInInfo>();
-                var accountInfo = await GetAccountInfo(signUpInfo.idToken);
-                var metadata = new UserMetadata(accountInfo.createdAt, accountInfo.lastLoginAt);
-                var user = new User(accountInfo.displayName, accountInfo.email, accountInfo.emailVerified,
-                                    false, metadata, null, accountInfo.photoUrl, null, null, 
-                                    signUpInfo.refreshToken, signUpInfo.localId);
-                return user;
+                var error = await response.Content.ReadAsAsync<Error>();
+                if (error != null)
+                    throw error.GetCorrespondingException();
+
+                throw new FirebaseAuthException();
             }
-            return null;
+
+            var signUpInfo = await response.Content.ReadAsAsync<SignInInfo>();
+            var accountInfo = await GetAccountInfo(signUpInfo.idToken);
+            var metadata = new UserMetadata(accountInfo.createdAt, accountInfo.lastLoginAt);
+            var user = new User(accountInfo.displayName, accountInfo.email, accountInfo.emailVerified,
+                                false, metadata, null, accountInfo.photoUrl, null, null,
+                                signUpInfo.refreshToken, signUpInfo.localId);
+            return user;
         } 
 
         public async Task<User> SignInWithEmailAndPassword(string email, string password)
         {
-            string url = Constants.SignInEndpoint + EndpointKeySuffix;
+            string url = Endpoints.SignIn + EndpointKeySuffix;
             var body = new { email, password, returnSecureToken = true };
             var response = await Client.PostAsJsonAsync(url, body);
-            if (response.IsSuccessStatusCode)
+            if (!response.IsSuccessStatusCode)
             {
-                var signInInfo = await response.Content.ReadAsAsync<SignInInfo>();
-                var accountInfo = await GetAccountInfo(signInInfo.idToken);
-                var metadata = new UserMetadata(accountInfo.createdAt, accountInfo.lastLoginAt);
-                var user = new User(accountInfo.displayName, accountInfo.email, accountInfo.emailVerified,
-                                    false, metadata, null, accountInfo.photoUrl, null, null,
-                                    signInInfo.refreshToken, signInInfo.localId);
-                return user;
+                var error = await response.Content.ReadAsAsync<Error>();
+                if (error != null)
+                    throw error.GetCorrespondingException();
+
+                throw new FirebaseAuthException();
             }
-            return null;
+
+            var signInInfo = await response.Content.ReadAsAsync<SignInInfo>();
+            var accountInfo = await GetAccountInfo(signInInfo.idToken);
+            var metadata = new UserMetadata(accountInfo.createdAt, accountInfo.lastLoginAt);
+            var user = new User(accountInfo.displayName, accountInfo.email, accountInfo.emailVerified,
+                                false, metadata, null, accountInfo.photoUrl, null, null,
+                                signInInfo.refreshToken, signInInfo.localId);
+            return user;
         }
 
         public async Task<User> SignInAnonymously()
         {
-            string url = Constants.SignUpEndpoint + EndpointKeySuffix;
+            string url = Endpoints.SignUp + EndpointKeySuffix;
             var body = new { returnSecureToken = true };
             var response = await Client.PostAsJsonAsync(url, body);
-            if (response.IsSuccessStatusCode)
+            if (!response.IsSuccessStatusCode)
             {
-                var signInInfo = await response.Content.ReadAsAsync<SignInInfo>();
-                var user = new User(null, null, false, true, null, null, null, null, null,
-                                    signInInfo.refreshToken, signInInfo.localId);
-                return user;
+                var error = await response.Content.ReadAsAsync<Error>();
+                if (error != null)
+                    throw error.GetCorrespondingException();
+
+                throw new FirebaseAuthException();
             }
-            return null;
+
+            var signInInfo = await response.Content.ReadAsAsync<SignInInfo>();
+            var user = new User(null, null, false, true, null, null, null, null, null,
+                                signInInfo.refreshToken, signInInfo.localId);
+            return user;
         }
 
         public async Task<bool> SendPasswordResetEmail(User user)
         {
-            string url = Constants.SendConfirmationEmailEndpoint + EndpointKeySuffix;
-            var body = new { requestType = Constants.PasswordResetRequest, email = user.Email };
+            string url = Endpoints.SendConfirmationEmail + EndpointKeySuffix;
+            var body = new { requestType = RequestTypes.PasswordReset, email = user.Email };
             var response = await Client.PostAsJsonAsync(url, body);
-            return response.IsSuccessStatusCode;
+            if (!response.IsSuccessStatusCode)
+            {
+                var error = await response.Content.ReadAsAsync<Error>();
+                if (error != null)
+                    throw error.GetCorrespondingException();
+
+                throw new FirebaseAuthException();
+            }
+            return true;
         }
 
         public async Task<bool> VerifyPasswordResetCorde(string code)
         {
-            string url = Constants.PasswordResetEndpoint + EndpointKeySuffix;
+            string url = Endpoints.PasswordReset + EndpointKeySuffix;
             var body = new { oobCode = code };
             var response = await Client.PostAsJsonAsync(url, body);
-            return response.IsSuccessStatusCode;
+            if (!response.IsSuccessStatusCode)
+            {
+                var error = await response.Content.ReadAsAsync<Error>();
+                if (error != null)
+                    throw error.GetCorrespondingException();
+
+                throw new FirebaseAuthException();
+            }
+            return true;
         }
 
         public async Task<bool> ConfirmPasswordReset(string code, string newPassword)
         {
-            string url = Constants.PasswordResetEndpoint + EndpointKeySuffix;
+            string url = Endpoints.PasswordReset + EndpointKeySuffix;
             var body = new { oobCode = code, newPassword };
             var response = await Client.PostAsJsonAsync(url, body);
-            return response.IsSuccessStatusCode;
+            if (!response.IsSuccessStatusCode)
+            {
+                var error = await response.Content.ReadAsAsync<Error>();
+                if (error != null)
+                    throw error.GetCorrespondingException();
+
+                throw new FirebaseAuthException();
+            }
+            return true;
         }
+
+
     }
 }
