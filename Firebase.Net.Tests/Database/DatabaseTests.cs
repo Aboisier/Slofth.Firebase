ca8a658f2f563a7e3f984a446152d5a37077e345
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2018-01-24 15:25:33 -0500

    Added test data and fixed configuration

---
+++ Firebase.Net.Tests/Database/DatabaseTests.cs
@@ -0,0 +1,43 @@
+﻿using Firebase.Net.Database;
+using NUnit.Framework;
+using System.Configuration;
+using System.Threading.Tasks;
+
+namespace PolyPaint.Tests.Services
+{
+    [TestFixture]
+    class FirebaseDatabaseServiceTests
+    {
+        private FirebaseHelper FirebaseHelper { get; set; }
+        private Database Database { get; set; }
+
+        private string DatabaseUrl => ConfigurationManager.AppSettings.Get("DatabaseUrl");
+        private string DatabaseSecret => ConfigurationManager.AppSettings.Get("DatabaseSecret");
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            FirebaseHelper = new FirebaseHelper();
+            Database = new Database(DatabaseUrl, () => DatabaseSecret);
+        }
+
+        [TearDown]
+        [OneTimeTearDown]
+        public async Task TearDown()
+        {
+            await FirebaseHelper.CleanUp();
+        }
+
+        [Test]
+        public async Task Testy()
+        {
+            // Arrange
+            await FirebaseHelper.UpdateRules("AllowEverything.json");
+
+            // Act
+            //Database.Ref("lel").Once();
+        }
+    }
+
+}
