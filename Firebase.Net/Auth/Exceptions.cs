d8fca9ce9d89bc4ceb6dc15268c0ff090f42ed9e
Rémi Pelletier <remi.pelletier@polymtl.ca>
2018-01-24 14:30:05 -0500

    Added Auth exceptions

---
+++ Firebase.Net/Auth/Exceptions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Firebase.Net.Auth
+{
+    public class EmailExistsException : Exception { }
+
+    public class OperationNotAllowedException : Exception { }
+
+    public class TooManyAttemptsException : Exception { }
+
+    public class EmailNotFoundException : Exception { }
+
+    public class InvalidPasswordException : Exception { }
+
+    public class UserDisabledException : Exception { }
+
+    public class InvalidIdpResponseException : Exception { }
+
+    public class ExpiredOobCodeException : Exception { }
+
+    public class InvalidOobCodeException : Exception { }
+
+    public class InvalidIdTokenException : Exception { }
+
+    public class WeakPasswordException : Exception { }
+
+    public class UserNotFoundException : Exception { }
+
+    public class CredentialTooOldException : Exception { }
+
+    public class TokenExpiredException : Exception { }
+
+    public class FederateUserIdAlreadyLinkedException : Exception { }
+}
