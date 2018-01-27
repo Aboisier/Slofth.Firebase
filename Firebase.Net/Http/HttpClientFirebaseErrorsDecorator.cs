1d03aebccdc81b3465fb917a701075f72cec492c
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2018-01-26 22:13:59 -0500

    Implemented HttpClientFacade decorator that handles firebase errors

---
+++ Firebase.Net/Http/HttpClientFirebaseErrorsDecorator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Firebase.Net.Http
+{
+    public class HttpClientFirebaseErrorsDecorator : HttpClientDecorator
+    {
+        public HttpClientFirebaseErrorsDecorator(IHttpClientFacade baseComponent) : base(baseComponent) { }
+
+        public override async Task<HttpResponseMessage> GetAsync(string url)
+        {
+            return await ExecuteHandledRequest(async () => await BaseComponent.GetAsync(url));
+        }
+
+        public override async Task<HttpResponseMessage> PostAsJsonAsync<T>(string url, T value)
+        {
+            return await ExecuteHandledRequest(async () => await BaseComponent.PostAsJsonAsync(url, value));
+        }
+
+        public override async Task<HttpResponseMessage> PutAsJsonAsync<T>(string url, T value)
+        {
+            return await ExecuteHandledRequest(async () => await BaseComponent.PutAsJsonAsync(url, value));
+        }
+
+        public override async Task<HttpResponseMessage> DeleteAsync(string url)
+        {
+            return await ExecuteHandledRequest(async () => await BaseComponent.DeleteAsync(url));
+        }
+
+        private async Task<HttpResponseMessage> ExecuteHandledRequest(Func<Task<HttpResponseMessage>> request)
+        {
+            var response = await request();
+            if (!response.IsSuccessStatusCode)
+            {
+                var error = await response.Content.ReadAsAsync<Error>();
+
+                if (error != null)
+                    throw error.GetCorrespondingException();
+
+                throw new FirebaseException();
+            }
+
+            return response;
+        }
+    }
+}
