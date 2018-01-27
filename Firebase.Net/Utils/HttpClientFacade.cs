d6eb331d180332fe17a2f8c5912b2ee4808e2adf
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2018-01-26 21:47:07 -0500

    Implemented façade for HttpClient

---
+++ Firebase.Net/Utils/HttpClientFacade.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Firebase.Net.Utils
+{
+    class HttpClientFacade
+    {
+        private HttpClient Client { get; set; }
+
+        public HttpClientFacade()
+        {
+            Client = new HttpClient();
+        }
+
+        public HttpClientFacade(Uri baseAddress)
+        {
+            Client = new HttpClient();
+            Client.BaseAddress = baseAddress;
+        }
+
+        public HttpClientFacade(HttpClient client)
+        {
+            Client = client;
+        }
+
+        public async Task<HttpResponseMessage> PostAsJsonAsync<T>(string url, T value)
+        {
+            return await Client.PostAsJsonAsync(url, value);
+        }
+
+        public async Task<HttpResponseMessage> PutAsJsonAsync<T>(string url, T value)
+        {
+            return await Client.PutAsJsonAsync(url, value);
+        }
+
+        public async Task<HttpResponseMessage> GetAsync(string url)
+        {
+            return await Client.GetAsync(url);
+        }
+
+        public async Task<HttpResponseMessage> PutAsJsonAsync(string url)
+        {
+            return await Client.DeleteAsync(url);
+        }
+    }
+}
