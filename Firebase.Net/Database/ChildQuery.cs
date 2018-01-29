b9a7b9b85388d2c865d7b0219865b62f78c30c25
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2018-01-29 12:04:01 -0500

    Added order by method

--- Firebase.Net/Database/ChildQuery.cs
+++ Firebase.Net/Database/ChildQuery.cs
@@ -1,8 +1,4 @@
 ﻿using Firebase.Net.Utils;
-using Newtonsoft.Json;
-using System;
-using System.Net.Http;
-using System.Threading.Tasks;
 
 namespace Firebase.Net.Database
 {
@@ -12,10 +8,33 @@ namespace Firebase.Net.Database
             : base(builder, name)
         { }
 
+        /// <summary>
+        /// Orders the data according to the specified property in order to filter it.
+        /// </summary>
+        /// <param name="property">The property used to sort. To access a nested property, use slashes (ex.: info/dimensions/height).</param>
         public FilterQuery OrderBy(string property)
         {
-            return new FilterQuery(UrlBuilder.AddParam("orderBy", property), Key);
+            return new FilterQuery(UrlBuilder.AddParam(Params.OrderBy, $"\"{property}\""), Key);
+        }
 
+        public FilterQuery OrderByKey()
+        {
+            return OrderBy("$key");
+        }
+
+        public FilterQuery OrderByPriority()
+        {
+            return OrderBy("$priority");
+        }
+
+        public FilterQuery OrderByValue()
+        {
+            return OrderBy("$value");
+        }
+
+        class Params
+        {
+            public static readonly string OrderBy = "orderBy";
         }
     }
 }
